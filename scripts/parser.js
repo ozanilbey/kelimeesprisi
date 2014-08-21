7a0046013cbe34710210bcf62a6b0b6d53abdf0b
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2014-08-22 02:59:48 +0300

    File format changed in script

--- scripts/parser.js
+++ scripts/parser.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
         for(i = 0; i < keys.length; i++) {
             postID = keys[i];
             postData = data[postID];
-            $('#postContainer').append('<div class="' + postData['type'] + ' post"><a href="https://www.facebook.com/photo.php?fbid=' + postID + '" target="_blank"><div class="wrapper"><img src="posts/' + postID + '.png" /></div></a><div class="credits"><b>Gönderen:</b> <a href="https://www.facebook.com/' + postData['source']['facebook_id'] + '" target="_blank">' + postData['source']['fullname'] + '</a></div></div>');
+            $('#postContainer').append('<div class="' + postData['type'] + ' post"><a href="https://www.facebook.com/photo.php?fbid=' + postID + '" target="_blank"><div class="wrapper"><img src="posts/' + postID + '.jpg" /></div></a><div class="credits"><b>Gönderen:</b> <a href="https://www.facebook.com/' + postData['source']['facebook_id'] + '" target="_blank">' + postData['source']['fullname'] + '</a></div></div>');
         }
         
         $('#postContainer').append('<p class="endnote">Her Hakkı erkektir ve erkek adam saklanmaz!<br><a class="bold" href="http://twitter.com/fasoadmn" target="_blank">fa$o admin</a> &amp; <a class="bold" href="http://twitter.com/yanciadmn" target="_blank">yancı admin</a></p>');
